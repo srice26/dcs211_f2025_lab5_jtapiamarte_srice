18fefc2fdc543b5187d39a90a3b4076e122c15a1
Sam Rice <samrice@bobcat-104-89.bates.edu>
2025-11-04 10:25:34 -0500

    adding files

---
+++ dcs211_lab5.py
@@ -0,0 +1,78 @@
+import numpy as np      # numpy is Python's "array" library
+import pandas as pd     # Pandas is Python's "data" library ("dataframe" == spreadsheet)
+import seaborn as sns   # yay for Seaborn plots!
+import matplotlib.pyplot as plt
+import random
+
+###########################################################################
+def drawDigitHeatmap(pixels: np.ndarray, showNumbers: bool = True) -> None:
+    ''' Draws a heat map of a given digit based on its 8x8 set of pixel values.
+    Parameters:
+        pixels: a 2D numpy.ndarray (8x8) of integers of the pixel values for
+                the digit
+        showNumbers: if True, shows the pixel value inside each square
+    Returns:
+        None -- just plots into a window
+    '''
+
+    (fig, axes) = plt.subplots(figsize = (4.5, 3))  # aspect ratio
+
+    rgb = (0, 0, 0.5)  # each in (0,1), so darkest will be dark blue
+    colormap = sns.light_palette(rgb, as_cmap=True)    
+    # all seaborn palettes: 
+    # https://medium.com/@morganjonesartist/color-guide-to-seaborn-palettes-da849406d44f
+
+    # plot the heatmap;  see: https://seaborn.pydata.org/generated/seaborn.heatmap.html
+    # (fmt = "d" indicates to show annotation with integer format)
+    sns.heatmap(pixels, annot = showNumbers, fmt = "d", linewidths = 0.5, \
+                ax = axes, cmap = colormap)
+    plt.show(block = False)
+
+###########################################################################
+def fetchDigit(df: pd.core.frame.DataFrame, which_row: int) -> tuple[int, np.ndarray]:
+    ''' For digits.csv data represented as a dataframe, this fetches the digit from
+        the corresponding row, reshapes, and returns a tuple of the digit and a
+        numpy array of its pixel values.
+    Parameters:
+        df: pandas data frame expected to be obtained via pd.read_csv() on digits.csv
+        which_row: an integer in 0 to len(df)
+    Returns:
+        a tuple containing the reprsented digit and a numpy array of the pixel
+        values
+    '''
+    digit  = int(round(df.iloc[which_row, 64]))
+    pixels = df.iloc[which_row, 0:64]   # don't want the rightmost rows
+    pixels = pixels.values              # converts to numpy array
+    pixels = pixels.astype(int)         # convert to integers for plotting
+    pixels = np.reshape(pixels, (8,8))  # makes 8x8
+    return (digit, pixels)              # return a tuple
+
+###################
+def main() -> None:
+    # for read_csv, use header=0 when row 0 is a header row
+    filename = 'digits.csv'
+    df = pd.read_csv(filename, header = 0)
+    print(df.head())
+    print(f"{filename} : file read into a pandas dataframe...")
+
+    num_to_draw = 5
+    for i in range(num_to_draw):
+        # let's grab one row of the df at random, extract/shape the digit to be
+        # 8x8, and then draw a heatmap of that digit
+        random_row = random.randint(0, len(df) - 1)
+        (digit, pixels) = fetchDigit(df, random_row)
+
+        print(f"The digit is {digit}")
+        print(f"The pixels are\n{pixels}")  
+        drawDigitHeatmap(pixels)
+        plt.show()
+
+    #
+    # OK!  Onward to knn for digits! (based on your iris work...)
+    #
+
+###############################################################################
+# wrap the call to main inside this if so that _this_ file can be imported
+# and used as a library, if necessary, without executing its main
+if __name__ == "__main__":
+    main()
